898ce1f96895b2a4a5b497ca7090fc12fb6b8706
jesus rlv <jesusrlv@iMac-de-jesus.local>
2022-12-20 01:15:44 -0600

    db catalogos

--- eventosUACYA.sql
+++ eventosUACYA.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
+-- version 4.5.2
+-- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 16-12-2022 a las 22:07:41
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 7.4.28
+-- Tiempo de generación: 20-12-2022 a las 08:15:20
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -44,7 +43,8 @@ CREATE TABLE `asistentes` (
 --
 
 INSERT INTO `asistentes` (`id`, `nombre`, `apellidos`, `tipo_sangre`, `semestre`, `unidad_academica`, `carrera`, `numero_control`, `evento`) VALUES
-(1, 'rodolfo', 'leaños', 0, 9, 3, 5, 450489, 10);
+(1, 'rodolfo', 'leaños', 0, 9, 3, 5, 450489, 10),
+(2, 'JesusRLV', 'Leaños Villegas', 7, 9, 1, 5, 450489, 12);
 
 -- --------------------------------------------------------
 
@@ -77,7 +77,103 @@ INSERT INTO `eventos` (`id`, `nombre`, `capacidad`, `fecha`, `lugar`, `activo`) 
 (9, 'EVENTO 9', 30, '2022-12-02', 'Auditorio FCA', 1),
 (10, 'EVENTO 9', 30, '2022-12-02', 'Auditorio FCA', 1),
 (11, 'Contabilidad', 10, '2022-12-04', 'Aula 1', 1),
-(12, 'Simpsium', 30, '2022-12-01', 'ANFECA', 1);
+(12, 'Simpsium', 30, '2022-12-01', 'ANFECA', 1),
+(13, 'Evento NOV', 21, '2022-11-01', 'Anfeca', 1),
+(14, 'Evento OCT', 9, '2022-10-01', 'FECA', 1),
+(15, 'Evento SEPT', 81, '2022-09-01', 'Anfeca', 1),
+(16, 'Evento DIC', 90, '2022-12-19', 'FECA', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programa`
+--
+
+CREATE TABLE `programa` (
+  `id` int(11) NOT NULL,
+  `programa` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `programa`
+--
+
+INSERT INTO `programa` (`id`, `programa`) VALUES
+(1, 'Licenciatura en contaduría'),
+(2, 'Licenciatura en administración'),
+(3, 'Maestría en administración'),
+(4, 'Maestría en impuestos'),
+(5, 'Doctorado en administración'),
+(6, 'Doctorado en impuestos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sangre`
+--
+
+CREATE TABLE `sangre` (
+  `id` int(11) NOT NULL,
+  `tipo_sangre` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sangre`
+--
+
+INSERT INTO `sangre` (`id`, `tipo_sangre`) VALUES
+(1, 'A positivo (A +)'),
+(2, 'A negativo (A-)'),
+(3, 'B positivo (B +)'),
+(4, 'B negativo (B-)'),
+(5, 'AB positivo (AB+)'),
+(6, 'AB negativo (AB-)'),
+(7, 'O positivo (O+)'),
+(8, 'O negativo (O-)');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `semestre`
+--
+
+CREATE TABLE `semestre` (
+  `id` int(11) NOT NULL,
+  `semestre` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `semestre`
+--
+
+INSERT INTO `semestre` (`id`, `semestre`) VALUES
+(1, 'Primer semestre'),
+(2, 'Segundo semestre'),
+(3, 'Tercer semestre'),
+(4, 'Cuarto semestre'),
+(5, 'Quinto semestre'),
+(6, 'Sexto semestre'),
+(7, 'Séptimo semestre'),
+(8, 'Octavo semestre'),
+(9, 'Noveno semestre');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `unidad_academica`
+--
+
+CREATE TABLE `unidad_academica` (
+  `id` int(11) NOT NULL,
+  `unidad_academica` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `unidad_academica`
+--
+
+INSERT INTO `unidad_academica` (`id`, `unidad_academica`) VALUES
+(1, 'Unidad Académica de Contaduría y Adminisitración');
 
 -- --------------------------------------------------------
 
@@ -109,6 +205,30 @@ ALTER TABLE `eventos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `programa`
+--
+ALTER TABLE `programa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sangre`
+--
+ALTER TABLE `sangre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `semestre`
+--
+ALTER TABLE `semestre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `unidad_academica`
+--
+ALTER TABLE `unidad_academica`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usr`
 --
 ALTER TABLE `usr`
@@ -122,21 +242,37 @@ ALTER TABLE `usr`
 -- AUTO_INCREMENT de la tabla `asistentes`
 --
 ALTER TABLE `asistentes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT de la tabla `programa`
+--
+ALTER TABLE `programa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `sangre`
+--
+ALTER TABLE `sangre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT de la tabla `semestre`
+--
+ALTER TABLE `semestre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT de la tabla `unidad_academica`
+--
+ALTER TABLE `unidad_academica`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `usr`
 --
 ALTER TABLE `usr`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
